f202feff3ff1be0f482b5148f30fd6f6d85817b8
AngelsDeveloper <johnny.matos777@gmail.com>
2021-09-08 02:08:16 -0300

    sistema de banco de dados adicionado

--- cadastro.lua
+++ cadastro.lua
@@ -5,11 +5,28 @@ local password = require "password"
 
 os.execute("clear")
 
-local user = {}
+local user = {
+    username = nil,
+    email = nil,
+    password = nil
+}
+
+local database = {
+    file = nil,
+    conteudo = nil,
+    data = nil
+}
+
+database.file = io.open("database.json", "r")
+database.conteudo = database.file:read("*a")
+database.data = json.decode(database.conteudo) or {}
+
 user.username = username.InputUsername()
 user.email = email.InputEmail()
 user.password = password.InputPassword()
 
-io.write("\n", "----- CADASTRO FEITO COM SUCESSO -----", "\n\n")
+database.data[#database.data + 1] = user
+database.file = io.open("database.json", "w+")
+database.file:write(json.encode(database.data))
 
-print(json.encode(user))
+io.write("\n", "----- CADASTRO FEITO COM SUCESSO -----", "\n\n")
